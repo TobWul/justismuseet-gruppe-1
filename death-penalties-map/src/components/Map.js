3e6747a7dbeb3174a6daed4c775fbcd31f8f1e99
eskilsen <eskil_hognestad@hotmail.com>
2020-01-14 10:28:24 +0100

    added mapbox

--- death-penalties-map/src/components/Map.js
+++ death-penalties-map/src/components/Map.js
@@ -1,13 +1,30 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
+import mapboxgl from 'mapbox-gl';
+
+mapboxgl.accessToken = 'pk.eyJ1IjoidG9iaWFzd3VsdmlrIiwiYSI6ImNqeHd1NG5jcjA0MGQzcG56MTc5dTN6cHQifQ.9Oejzg6ki693vbomStWxVg'
+
 
 const Map = () => {
   const [lng, setLng] = useState(65.094);
   const [lat, setLat] = useState(18.716);
   const [zoom, setZoom] = useState(3.71);
+
+  let mapContainer = useRef(null);
+
   useEffect(() => {
-    // Mapbox setupscript
+      const map = new mapboxgl.Map({
+      container: mapContainer,
+      style: 'mapbox://styles/tobiaswulvik/ck5ckdnwg0c1a1cnst608dhif',
+      center: [lng, lat],
+      zoom: zoom
+      });
   }, []);
-  return <div>Kart</div>;
+
+  return (
+  <div> 
+    <div ref= {el => mapContainer = el}>Kart</div>
+  </div >
+  )
 };
 
 export default Map;
