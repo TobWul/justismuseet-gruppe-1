feb5fa0a5c97b8d874a866022e2360356e0cbe81
Tobias Wulvik <tobias.wulvik@gmail.com>
2020-01-14 10:10:59 +0100

    setting up map component

--- death-penalties-map/src/components/Map.js
+++ death-penalties-map/src/components/Map.js
@@ -1,6 +1,12 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const Map = () => {
+  const [lng, setLng] = useState(65.094);
+  const [lat, setLat] = useState(18.716);
+  const [zoom, setZoom] = useState(3.71);
+  useEffect(() => {
+    // Mapbox setupscript
+  }, []);
   return <div>Kart</div>;
 };
 
